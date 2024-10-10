470faddd80b3805b0fa89526ca0aaf18ac86a919
RoboXGamer <roboxgamer52@gmail.com>
2024-10-10 13:56:30 +0530

    Add Item Capabilities to generator block

--- src/main/java/net/roboxgamer/tutorialmod/block/entity/custom/ResourceGeneratorBlockEntity.java
+++ src/main/java/net/roboxgamer/tutorialmod/block/entity/custom/ResourceGeneratorBlockEntity.java
@@ -7,10 +7,14 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import net.roboxgamer.tutorialmod.TutorialMod;
@@ -44,7 +48,42 @@ public class ResourceGeneratorBlockEntity extends BlockEntity {
       return stack;
     }
   };
-  CustomItemStackHandler filterInv = new CustomItemStackHandler(1);
+  CustomItemStackHandler filterInv = new CustomItemStackHandler(1){
+    @Override
+    public int getSlotLimit(int slot) {
+      return 1;
+    }
+  };
+  
+  private LazyOptional<IItemHandler> lazyInvHandler = LazyOptional.empty();
+  
+  @Override
+  public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+    if (cap == ForgeCapabilities.ITEM_HANDLER) {
+      return lazyInvHandler.cast();
+    }
+    return super.getCapability(cap, side);
+  }
+  
+  public SimpleContainer getInvContainer() {
+    var t = new SimpleContainer(this.inv.getSlots());
+    for (int i = 0; i < this.inv.getSlots(); i++) {
+      t.setItem(i, this.inv.getStackInSlot(i));
+    }
+    return t;
+  }
+  
+  @Override
+  public void onLoad() {
+    super.onLoad();
+    lazyInvHandler = LazyOptional.of(() -> inv);
+  }
+  
+  @Override
+  public void invalidateCaps() {
+    super.invalidateCaps();
+    lazyInvHandler.invalidate();
+  }
   
   public void setItem(ItemStack stack) {
     this.filterInv.setStackInSlot(0, stack);
